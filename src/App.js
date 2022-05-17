43b34acd0e35542db708e9624242f98140d91029
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-05-17 16:56:24 +0300

    Todo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import From from './components/Form.jsx';
+
+function App() {
+  return (
+    <div className="md:container md:mx-auto">
+      <From />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        0.1: "0.063rem",
+      },
+      colors: {
+        "brand-dark-color": "#424245",
+        "brand-white-color": "#f5f5f7",
+        "secondary-brand-color": "#7849f7",
+        "brand-yellow": "#ffd300",
+        "primary-white-color": "rgba(255, 255, 255, 0.92)"
+      },
+      variants: {
+        display: ["responsive", "group-hover", "group-focus"],
+      },
+    },
+  },
+  plugins: [],
+};
